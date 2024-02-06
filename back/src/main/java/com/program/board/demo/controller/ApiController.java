3ab07e292552c847934b541b75fb7860c0904641
Matheus Fidelis <mfds@a.recife.ifpe.edu.br>
2024-02-06 16:32:14 -0300

    endpoint tasks

--- back/src/main/java/com/program/board/demo/controller/ApiController.java
+++ back/src/main/java/com/program/board/demo/controller/ApiController.java
@@ -2,10 +2,12 @@ package com.program.board.demo.controller;
 
 import com.program.board.demo.model.Feature;
 import com.program.board.demo.model.dtos.FeatureDto;
+import com.program.board.demo.model.dtos.TaskDto;
 import com.program.board.demo.model.dtos.TeamDto;
 import jakarta.validation.Valid;
 import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
+import org.springframework.core.task.TaskDecorator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -26,24 +28,20 @@ public class ApiController {
 
     @PutMapping("/feature/update-feature")
     public ResponseEntity<?> updateFeature(@RequestBody FeatureDto featureDto) {
-
         return ResponseEntity.ok("ok");
     }
 
     @GetMapping("/feature")
     ResponseEntity<?> getAllFeatures() {
         List<Feature> featuresList = null;
-
         return ResponseEntity.ok("ok");
     }
     @GetMapping("/feature/{id}")
     ResponseEntity<?> getOneFeature(@PathVariable(value = "id") Long id) {
-
         return ResponseEntity.ok("ok");
     }
     @DeleteMapping("/feature/delete-feature/{id}")
     ResponseEntity<?> deleteFeature(@PathVariable(value = "id") Long id) {
-
         return ResponseEntity.ok("ok");
     }
 
@@ -53,22 +51,40 @@ public class ApiController {
     }
     @GetMapping("/team")
     ResponseEntity<?> getTeams(){
-
         return ResponseEntity.ok("ok");
     }
     @GetMapping("/team/{id}")
     ResponseEntity<?> getTeamById(@PathVariable Long id){
-
         return ResponseEntity.ok("ok");
     }
     @PutMapping("/team/alter-team")
     public ResponseEntity<?> getTeams(@RequestBody TeamDto team){
-
         return ResponseEntity.ok("ok");
     }
     @DeleteMapping("/team/delete-team/{id}")
     public ResponseEntity<?> deleteTeam(@PathVariable Long id){
         return ResponseEntity.ok(HttpStatus.resolve(200));
+    }
+
+    @GetMapping("/task")
+    public ResponseEntity<?> getTasks(){
+        return ResponseEntity.ok("Ok");
+    }
+    @GetMapping("/task/{id}")
+    public ResponseEntity<?> getTaskById(@PathVariable Long id){
+        return ResponseEntity.ok("OK");
+    }
+    @PostMapping("/task/create-task")
+    public ResponseEntity<?> createTask(@RequestBody TaskDto task){
+        return ResponseEntity.ok("OK");
+    }
+    @PutMapping("/task/alter-task")
+    public ResponseEntity<?> alterTask(@RequestBody TaskDto task){
+        return ResponseEntity.ok("OK");
+    }
+    @DeleteMapping("/task/delete-task/{id}")
+    public ResponseEntity<?> deleteTask(@PathVariable Long id){
+        return ResponseEntity.ok("OK");
     }
 
 }
--- back/src/main/java/com/program/board/demo/model/dtos/FeatureDto.java
+++ back/src/main/java/com/program/board/demo/model/dtos/FeatureDto.java
@@ -3,19 +3,15 @@ package com.program.board.demo.model.dtos;
 import com.program.board.demo.model.Feature;
 import com.program.board.demo.model.Task;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
 @Component
-@Getter
-@Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 public class FeatureDto {
 
     private Long idFeature;
