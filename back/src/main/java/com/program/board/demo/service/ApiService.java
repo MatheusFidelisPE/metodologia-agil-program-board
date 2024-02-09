647fc04c03f90261b30c7a2c1d22153bf205251e
Luiz <luiz.m.franca1@gmail.com>
2024-02-08 23:12:52 -0300

    [Add] Feature Functions

--- back/src/main/java/com/program/board/demo/service/ApiService.java
+++ back/src/main/java/com/program/board/demo/service/ApiService.java
@@ -1,8 +1,43 @@
 package com.program.board.demo.service;
 
+import com.program.board.demo.repository.FeatureRepository;
+import com.program.board.demo.model.Feature;
+import com.program.board.demo.model.dtos.FeatureDto;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.BeanUtils;
+
+import java.util.List;
+import java.util.Optional;
 
 // Regras de negócio
 @Service
 public class ApiService {
+    FeatureRepository featureRepository;
+
+    public Feature saveFeature(FeatureDto featureDto) {
+        var featureModel = new Feature();
+        BeanUtils.copyProperties(featureDto, featureModel);
+        return featureRepository.save(featureModel);
+    }
+
+    public Object updateFeature(Long id, FeatureDto featureDto) {
+        Optional<Feature> feature = featureRepository.findById(id);
+        if(feature.isEmpty()) {
+            return "Feature not found.";
+        }
+        var featureModel = feature.get();
+        BeanUtils.copyProperties(featureDto, featureModel);
+        return featureRepository.save(featureModel);
+    }
+
+    public List<Feature> getAllFeatures() {
+        return featureRepository.findAll();
+    }
+    Object getOneFeature(Long id) {
+        Optional<Feature> feature = featureRepository.findById(id);
+        if(feature.isEmpty()) {
+            return "Feature not found.";
+        }
+        return feature.get();
+    }
 }
